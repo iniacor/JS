91918cf9d3e9e7964238d6f07a82d54f1ac1ac6f
ihor <evroigor86@gmail.com>
2022-06-21 14:43:32 +0300

    add tasks

---
+++ loops/task1/index.js
@@ -0,0 +1,8 @@
+const n = 150;
+let j = 0;
+let sum = 0;
+while (j <= n) {
+  sum += j;
+  j++;
+}
+console.log(sum);
---
+++ loops/task2/index.js
@@ -0,0 +1,13 @@
+// put your code here
+const m = 1;
+const n = 10;
+let i = 1;
+let result = 1;
+while (i <= n) {
+  if (i % 2 === 1) {
+    result *= i;
+  }
+  i++;
+}
+console.log(result);
+// console.log(result);
