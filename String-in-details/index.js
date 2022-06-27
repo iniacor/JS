bde61d7ccc38c3d7b6172c4320da5d02436e8157
ihor <evroigor86@gmail.com>
2022-06-27 17:54:54 +0300

    add second task solution

--- String-in-details/index.js
+++ String-in-details/index.js
@@ -15,3 +15,49 @@
 //   return strArr.join('\n');
 // };
 // console.log(splitText('sdsfvmlsdssdsdmxzv', 4));
+//Сортируем телефонную книгу
+const arr = [
+  {
+    name: 'Tom',
+    phoneNumber: '777-77-77',
+  },
+  {
+    name: 'Bob',
+    phoneNumber: '725-74-27',
+  },
+  {
+    name: 'Mary',
+    phoneNumber: '737-75-70',
+  },
+  {
+    name: 'Alisa',
+    phoneNumber: '717-72-87',
+  },
+  {
+    name: 'Dilan',
+    phoneNumber: '767-57-13',
+  },
+];
+//рабочая версия
+// const sortContacts = (arr, direction) => {
+//   if (!Array.isArray(arr)) {
+//     return null;
+//   }
+//   const result = arr.sort((a, b) => {
+//     if (direction === false) {
+//       return b.name.localeCompare(a.name);
+//     } else return a.name.localeCompare(b.name);
+//   });
+//   return result;
+// };
+//Сокращенная версия
+// const sortContacts = (arr, direction) =>
+//   Array.isArray(arr)
+//     ? arr.sort((a, b) =>
+//         direction === false
+//           ? b.name.localeCompare(a.name)
+//           : a.name.localeCompare(b.name)
+//       )
+//     : null;
+
+// console.log(sortContacts(arr));
