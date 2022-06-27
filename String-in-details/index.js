f2662163fde9186a8eb1ae928a818a0a4f9d8085
ihor <evroigor86@gmail.com>
2022-06-27 10:13:29 +0300

    add first task solution

--- String-in-details/index.js
+++ String-in-details/index.js
@@ -1,17 +1,17 @@
-const splitText = (str, length = 10) => {
-  if (typeof str !== typeof '') {
-    return null;
-  }
-  const strArr = [];
-  let startPosition = 0;
-  while (true) {
-    let chunk = str.substr(startPosition, length);
-    if (chunk.length === 0) {
-      break;
-    }
-    strArr.push(chunk[0].toUpperCase() + chunk.slice(1));
-    startPosition += length;
-  }
-  return strArr.join('\n');
-};
-console.log(splitText('sdsfvmlsdssdsdmxzv', 4));
+// const splitText = (str, length = 10) => {
+//   if (typeof str !== typeof '') {
+//     return null;
+//   }
+//   const strArr = [];
+//   let startPosition = 0;
+//   while (true) {
+//     let chunk = str.substr(startPosition, length);
+//     if (chunk.length === 0) {
+//       break;
+//     }
+//     strArr.push(chunk[0].toUpperCase() + chunk.slice(1));
+//     startPosition += length;
+//   }
+//   return strArr.join('\n');
+// };
+// console.log(splitText('sdsfvmlsdssdsdmxzv', 4));
