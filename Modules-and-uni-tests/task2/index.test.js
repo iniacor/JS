f7f17d16d1dcc23e7cf8b23edc99cc57f9f467ac
ihor <evroigor86@gmail.com>
2022-06-29 09:33:44 +0300

    add change in valeu name

--- Modules-and-uni-tests/task2/index.test.js
+++ Modules-and-uni-tests/task2/index.test.js
@@ -1,6 +1,6 @@
 import getSum, { getSquaredNumbers, getOddNumbers } from './calculator';
 it('Should get squared numbers', () => {
-  const result = getSquaredNumbers([1, 2, 3]);
+  const result = getSquaredArray([1, 2, 3]);
   expect(result).toEqual([1, 4, 9]);
 });
 it('Should keep odd numbers only', () => {
