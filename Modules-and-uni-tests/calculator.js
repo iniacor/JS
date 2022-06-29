3a69ed439d0a0272f1db0bb24fae5d5044fede9b
ihor <evroigor86@gmail.com>
2022-06-29 09:31:29 +0300

    add task2 solution

--- Modules-and-uni-tests/calculator.js
+++ Modules-and-uni-tests/calculator.js
@@ -1,4 +1,4 @@
-let arr = [2, 4, 6, 7, 3, 5];
-export const getEvenNumbers = (arr) => arr.filter((num) => num % 2 === 0);
+// let arr = [2, 4, 6, 7, 3, 5];
+// export const getEvenNumbers = (arr) => arr.filter((num) => num % 2 === 0);
 
-console.log(getEvenNumbers(arr));
+// console.log(getEvenNumbers(arr));
---
+++ Modules-and-uni-tests/task2/index.test.js
@@ -0,0 +1,13 @@
+import getSum, { getSquaredNumbers, getOddNumbers } from './calculator';
+it('Should get squared numbers', () => {
+  const result = getSquaredNumbers([1, 2, 3]);
+  expect(result).toEqual([1, 4, 9]);
+});
+it('Should keep odd numbers only', () => {
+  const result = getOddNumbers([1, 2, 3, 4, 5]);
+  expect(result).toEqual([1, 3, 5]);
+});
+it('Should get sum of numbers', () => {
+  const result = getSum(8, 4);
+  expect(result).toEqual(12);
+});
