c33c05eab1b2187c9fe6d2fa8520a2f552d232a3
ihor <evroigor86@gmail.com>
2022-07-06 14:19:51 +0300

    add prototype lesson

---
+++ Prototypes-Inheritance/task1/index.js
@@ -0,0 +1,28 @@
+//put your code here
+const vehicle = {
+  name: 'Argo',
+  move() {
+    console.log(`${this.name} is moving`);
+  },
+  stop() {
+    console.log(`${this.name} stopped`);
+  },
+};
+
+const ship = {
+  startMachine() {
+    console.log(`${this.name} lifting anchor up`);
+    this.move();
+  },
+  stopMachine() {
+    this.stop();
+    console.log(`${this.name} lifting anchor down`);
+  },
+  __proto__: vehicle,
+};
+
+// --test data
+// vehicle.move();
+// vehicle.stop();
+// ship.startMachine();
+// ship.stopMachine();
