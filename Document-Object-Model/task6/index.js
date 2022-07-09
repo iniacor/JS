310afc9de0539f06d5bf9a2be15d6a625edf393d
ihor <evroigor86@gmail.com>
2022-07-09 14:51:20 +0300

    add changes

--- Document-Object-Model/task6/index.js
+++ Document-Object-Model/task6/index.js
@@ -1,7 +1,7 @@
 export const setButton = (buttonText) => {
   const bodyElem = document.querySelector('body');
   bodyElem.innerHTML = `<button>${buttonText}</button>`;
-  bodyElem.textContent = '<button>buttonText</button>';
+  // bodyElem.textContent = '<button>buttonText</button>';
 };
 // -test Data
 // setButton('push me');
