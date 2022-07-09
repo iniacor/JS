60c1f0c844d6c264405ae28f4cbaed782f3dcf5e
ihor <evroigor86@gmail.com>
2022-07-09 13:13:17 +0300

    add change

--- Document-Object-Model/task3/index.js
+++ Document-Object-Model/task3/index.js
@@ -1,6 +1,14 @@
+export const getItemsList = () => {
+  const elementsList = document.querySelectorAll('.technology');
+  console.dir(elementsList);
+  return elementsList;
+};
+getItemsList();
+
 export const getItemsArray = () => {
   const elementsArray = document.querySelectorAll('.tool');
   console.dir(elementsArray);
   const NodeList = Array.from(elementsArray);
   return NodeList;
 };
+getItemsArray();
