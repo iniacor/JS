644b3a94e128597f016630f214af1c06327b96fe
ihor <evroigor86@gmail.com>
2022-07-09 15:13:25 +0300

    add create btn soluyion

---
+++ Document-Object-Model/task8/index.js
@@ -0,0 +1,9 @@
+export const createButton = (buttonText) => {
+  const body = document.querySelector('body');
+  const button = document.createElement('button');
+  button.textContent = `${buttonText}`;
+  body.append(button);
+};
+
+// --test Data
+createButton('push me');
