1d85aea45e336cdc326a3d4bb8fd80f75ee17d42
ihor <evroigor86@gmail.com>
2022-07-09 17:26:18 +0300

    add change

---
+++ Document-Object-Model/task11/index.js
@@ -0,0 +1,15 @@
+export const manageClasses = () => {
+  const one = document.querySelector('.one');
+  one.classList.add('selected');
+  const two = document.querySelector('.two');
+  two.classList.remove('selected');
+  const three = document.querySelector('.three');
+  three.classList.toggle('three_done');
+  const four = document.querySelector('.four');
+  if ('some-class') {
+    four.classList.add('another-class');
+  }
+};
+
+// --test Data
+// manageClasses();
