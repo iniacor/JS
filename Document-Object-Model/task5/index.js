ff58e988aefa94b2fa0b112c8790756409db53f1
ihor <evroigor86@gmail.com>
2022-07-09 14:27:25 +0300

    add hw solution

---
+++ Document-Object-Model/task5/index.js
@@ -0,0 +1,6 @@
+export const setTitle = (text) => {
+  const titleElem = document.querySelector('.title');
+  titleElem.textContent = text;
+};
+// -- test data
+// setTitle("I'm fucking good Front-End developer");
