c97df542f12b3a4fd51383a5d39668252830cedb
ihor <evroigor86@gmail.com>
2022-07-11 12:42:03 +0300

    add change

--- Events/task4/index.js
+++ Events/task4/index.js
@@ -2,4 +2,4 @@ const checkBox = document.querySelector('.task-status');
 const cbStatus = () => {
   console.log(checkBox.checked);
 };
-checkBox.addEventListener('click', cbStatus);
+checkBox.addEventListener('change', cbStatus);
