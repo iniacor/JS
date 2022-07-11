124369234188b5fde7706fcd97439a8a2106b308
ihor <evroigor86@gmail.com>
2022-07-11 17:44:46 +0300

    add task solution

--- Events/task7/index.js
+++ Events/task7/index.js
@@ -1,5 +1,38 @@
-const btn = document.querySelector('.create-btn');
-const alert = () => {
-  window.alert('done');
+// через foreach
+const btnJs = document.querySelectorAll('.btn');
+const handleClick = (event) => {
+  console.log(event.target.textContent);
 };
-btn.addEventListener('click', alert);
+btnJs.forEach((btn) => {
+  btn.addEventListener('click', handleClick);
+});
+
+// через map
+// const btnJs = [...document.querySelectorAll('.btn')];
+
+// const handleClick = (event) => {
+//   console.log(event.target.textContent);
+// };
+// btnJs.map((btn) => {
+//   return btn.addEventListener('click', handleClick);
+// });
+// через делегирование события
+// const body = document.querySelector('body');
+// const handleClick = () => {
+//   console.log(event.target.textContent);
+// };
+
+// body.addEventListener('click', function (event) {
+//   if (event.target.closest('.btn')) {
+//     handleClick();
+//   }
+// });
+// через делегирование события сокращенно
+// const body = document.querySelector('body');
+// const handleClick = (event) => {
+//   if (event.target.closest('.btn')) {
+//     console.log(event.target.textContent);
+//   }
+// };
+
+// body.addEventListener('click', handleClick);
