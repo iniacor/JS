4354da52c57253f1310f3d32295da010c7bb2a3a
ihor <evroigor86@gmail.com>
2022-07-12 22:03:39 +0300

    add stadium task solution

--- Events/task12/index.js
+++ Events/task12/index.js
@@ -1,17 +1,64 @@
-// через делегирование
-// const pagination = document.querySelector('.pagination');
-// const handleClick = (event) => {
-//   if (event.target.closest('.pagination__page')) {
-//     console.log(event.target.dataset.pageNumber);
-//   }
-// };
-// pagination.addEventListener('click', handleClick);
+const generateNumbersRange = (from, to) => {
+  const result = [];
 
-// через foreach
-const paginationPgs = document.querySelectorAll('.pagination__page');
-const handleClick = (event) => {
-  console.log(event.target.dataset.pageNumber);
+  for (let i = from; i <= to; i += 1) {
+    result.push(i);
+  }
+  return result;
 };
-paginationPgs.forEach((el) => {
-  el.addEventListener('click', handleClick);
-});
+
+const getLineSeats = () => {
+  return generateNumbersRange(1, 10)
+    .map(
+      (seatNumber) =>
+        `<div 
+      class="sector__seat" 
+      data-seat-number="${seatNumber}"
+      ></div>`
+    )
+    .join('');
+};
+const getSectorLines = () => {
+  const seatsString = getLineSeats();
+  return generateNumbersRange(1, 10)
+    .map(
+      (lineNumber) =>
+        `<div 
+      class="sector__line" 
+      data-line-number="${lineNumber}"
+      >${seatsString}</div>`
+    )
+    .join('');
+};
+const arenaElem = document.querySelector('.arena');
+
+const renderArena = () => {
+  const linesString = getSectorLines();
+  const sectorsString = generateNumbersRange(1, 3)
+    .map(
+      (sectorNumber) =>
+        `<div 
+      class="sector" 
+      data-sector-number="${sectorNumber}"
+      >${linesString}</div>`
+    )
+    .join('');
+
+  arenaElem.innerHTML = sectorsString;
+};
+
+const onSeatSelect = (event) => {
+  const isSeat = event.target.classList.contains('sector__seat');
+  if (!isSeat) {
+    return;
+  }
+  const seatNum = event.target.dataset.seatNumber;
+  const lineNum = event.target.closest('.sector__line').dataset.lineNumber;
+  const sectorNum = event.target.closest('.sector').dataset.sectorNumber;
+
+  const selectedSeatElem = document.querySelector('.board__selected-seat');
+  selectedSeatElem.textContent = `S ${sectorNum} - L ${lineNum} - S ${seatNum}`;
+};
+
+arenaElem.addEventListener('click', onSeatSelect);
+renderArena();
