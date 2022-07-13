c922d3950bb852a2008b171fd33365075c851359
ihor <evroigor86@gmail.com>
2022-07-13 12:04:53 +0300

    add change

--- Events/task13/index.js
+++ Events/task13/index.js
@@ -1,17 +1,34 @@
-// через делегирование
-// const pagination = document.querySelector('.pagination');
-// const handleClick = (event) => {
-//   if (event.target.closest('.pagination__page')) {
-//     console.log(event.target.dataset.pageNumber);
-//   }
-// };
-// pagination.addEventListener('click', handleClick);
+const tasks = [
+  { text: 'Buy milk', done: false },
+  { text: 'Pick up Tom from airport', done: false },
+  { text: 'Visit party', done: false },
+  { text: 'Visit doctor', done: true },
+  { text: 'Buy meat', done: true },
+];
 
-// через foreach
-const paginationPgs = document.querySelectorAll('.pagination__page');
-const handleClick = (event) => {
-  console.log(event.target.dataset.pageNumber);
+const listElem = document.querySelector('.list');
+
+const renderTasks = (tasksList) => {
+  const tasksElems = tasksList
+    .sort((a, b) => a.done - b.done)
+    .map(({ text, done }) => {
+      const listItemElem = document.createElement('li');
+      listItemElem.classList.add('list__item');
+      const checkbox = document.createElement('input');
+      checkbox.setAttribute('type', 'checkbox');
+      checkbox.checked = done;
+      checkbox.classList.add('list__item-checkbox');
+      if (done) {
+        listItemElem.classList.add('list__item_done');
+      }
+      listItemElem.append(checkbox, text);
+
+      return listItemElem;
+    });
+
+  listElem.append(...tasksElems);
 };
-paginationPgs.forEach((el) => {
-  el.addEventListener('click', handleClick);
-});
+
+renderTasks(tasks);
+
+// put your code here
