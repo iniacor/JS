c26fd105fce3d8b484ac3e96c28b08ed532171f6
ihor <evroigor86@gmail.com>
2022-07-14 23:18:55 +0300

    add change

--- Browser-local-storage/task1/index.js
+++ Browser-local-storage/task1/index.js
@@ -1,18 +1,15 @@
 // -test Data
-localStorage.clear();
-localStorage.setItem('name', JSON.stringify('Tom'));
-localStorage.setItem('age', JSON.stringify(17));
-localStorage.setItem('data', JSON.stringify([1, 2, 'Karl']));
-localStorage.setItem('users', JSON.stringify({ name: 'Petya' }));
-localStorage.setItem('height', JSON.stringify(null));
-localStorage.setItem('isMarried', JSON.stringify(false));
+// localStorage.clear();
+// localStorage.setItem('name', JSON.stringify('Tom'));
+// localStorage.setItem('age', JSON.stringify(17));
+// localStorage.setItem('data', JSON.stringify([1, 2, 'Karl']));
+// localStorage.setItem('users', JSON.stringify({ name: 'Petya' }));
+// localStorage.setItem('height', JSON.stringify(null));
+// localStorage.setItem('isMarried', JSON.stringify(false));
 
 export const getLocalStorageData = () => {
   return Object.entries(localStorage).reduce((acc, [key, value]) => {
-    return {
-      ...acc,
-      [key]: JSON.parse(value),
-    };
+    return { ...acc, [key]: JSON.parse(value) };
   }, {});
 };
-console.log(getLocalStorageData());
+// console.log(getLocalStorageData());
