99e59c2b28022ae78ae0df78a0d2612d01146f7b
ihor <evroigor86@gmail.com>
2022-07-14 12:59:24 +0300

    add hw task solution

---
+++ Working-with-dates/task3/index.js
@@ -0,0 +1,42 @@
+// input obj,obj
+// output string: nDays nHours nMinutes nSecond
+export const getDiff = (startDate, endDate) => {
+  const utc1 = Date.UTC(
+    startDate.getFullYear(),
+    startDate.getMonth(),
+    startDate.getDate(),
+    startDate.getHours(),
+    startDate.getMinutes(),
+    startDate.getSeconds()
+  );
+  const utc2 = Date.UTC(
+    endDate.getFullYear(),
+    endDate.getMonth(),
+    endDate.getDate(),
+    endDate.getHours(),
+    endDate.getMinutes(),
+    endDate.getSeconds()
+  );
+  const duration = Math.floor(utc2 - utc1);
+  const days = parseInt(duration / (1000 * 60 * 60 * 24));
+  const hours = parseInt((duration / (1000 * 60 * 60)) % 24);
+  const minutes = parseInt((duration / (1000 * 60)) % 60);
+  const seconds = parseInt((duration / 1000) % 60);
+  return `${days}d ${hours}h ${minutes}m ${seconds}s`;
+};
+
+const startD = new Date();
+const endD = new Date(2017, 4, 13);
+const endD2 = new Date(2024, 3, 18);
+console.log(getDiff(startD, endD));
+console.log(getDiff(startD, endD2));
+console.log(startD);
+console.log(endD);
+
+// function msToTime(duration) {
+//   const days = parseInt(duration / (1000 * 60 * 60 * 24));
+//   const hours = parseInt((duration / (1000 * 60 * 60)) % 24);
+//   const minutes = parseInt((duration / (1000 * 60)) % 60);
+//   const seconds = parseInt((duration / 1000) % 60);
+//   return `${days}d ${hours}h ${minutes}m ${seconds}s`;
+// }
