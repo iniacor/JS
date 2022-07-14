b47675ac0936ec3d0d1935f51def88edab7977a6
ihor <evroigor86@gmail.com>
2022-07-14 18:14:54 +0300

    add change

---
+++ Pesochnitsa/dastruction/index.js
@@ -0,0 +1,24 @@
+' use strict ';
+function run() {
+  var a = 0;
+
+  if (a == 1) {
+    return c;
+  }
+
+  for (let i = 1; i < 10; i++) {
+    console.log(i);
+  }
+
+  return (d = a);
+}
+
+run();
+function run() {
+  var a = 0,
+    b = 1;
+
+  if (a == b) {
+    return c;
+  }
+}
