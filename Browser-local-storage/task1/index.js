c098a733ddedb436e52f14bd9b104de88b6751d5
ihor <evroigor86@gmail.com>
2022-07-14 23:23:18 +0300

    add try catch

--- Browser-local-storage/task1/index.js
+++ Browser-local-storage/task1/index.js
@@ -1,15 +1,26 @@
 // -test Data
-// localStorage.clear();
-// localStorage.setItem('name', JSON.stringify('Tom'));
-// localStorage.setItem('age', JSON.stringify(17));
-// localStorage.setItem('data', JSON.stringify([1, 2, 'Karl']));
-// localStorage.setItem('users', JSON.stringify({ name: 'Petya' }));
-// localStorage.setItem('height', JSON.stringify(null));
-// localStorage.setItem('isMarried', JSON.stringify(false));
+localStorage.clear();
+localStorage.setItem('name', JSON.stringify('Tom'));
+localStorage.setItem('age', JSON.stringify(17));
+localStorage.setItem('data', JSON.stringify([1, 2, 'Karl']));
+localStorage.setItem('users', JSON.stringify({ name: 'Petya' }));
+localStorage.setItem('height', JSON.stringify(null));
+localStorage.setItem('isMarried', JSON.stringify(false));
 
+// export const getLocalStorageData = () => {
+//   return Object.entries(localStorage).reduce((acc, [key, value]) => {
+//     return { ...acc, [key]: JSON.parse(value) };
+//   }, {});
+// };
 export const getLocalStorageData = () => {
   return Object.entries(localStorage).reduce((acc, [key, value]) => {
-    return { ...acc, [key]: JSON.parse(value) };
+    let newValue;
+    try {
+      newValue = JSON.parse(value);
+    } catch (error) {
+      newValue = value;
+    }
+    return { ...acc, [key]: newValue };
   }, {});
 };
-// console.log(getLocalStorageData());
+console.log(getLocalStorageData());
