814e3b500b8a0875ed3075de0d3397e59fddcec8
ihor <evroigor86@gmail.com>
2022-07-14 23:15:00 +0300

    add first task solution

---
+++ Browser-local-storage/task1/index.js
@@ -0,0 +1,18 @@
+// -test Data
+localStorage.clear();
+localStorage.setItem('name', JSON.stringify('Tom'));
+localStorage.setItem('age', JSON.stringify(17));
+localStorage.setItem('data', JSON.stringify([1, 2, 'Karl']));
+localStorage.setItem('users', JSON.stringify({ name: 'Petya' }));
+localStorage.setItem('height', JSON.stringify(null));
+localStorage.setItem('isMarried', JSON.stringify(false));
+
+export const getLocalStorageData = () => {
+  return Object.entries(localStorage).reduce((acc, [key, value]) => {
+    return {
+      ...acc,
+      [key]: JSON.parse(value),
+    };
+  }, {});
+};
+console.log(getLocalStorageData());
