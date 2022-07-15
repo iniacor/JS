f470748a8bbb21082a2dcc4bbe39a2076452f1ed
ihor <evroigor86@gmail.com>
2022-07-15 15:02:27 +0300

    add change

---
+++ Browser-local-storage/task3/src/scripts/createTask.js
@@ -0,0 +1,25 @@
+import { renderTasks } from './renderer.js';
+import { getItem, setItem } from './storage.js';
+
+export const onCreateTask = () => {
+  const taskTitleInputElem = document.querySelector('.task-input');
+
+  const text = taskTitleInputElem.value;
+
+  if (!text) {
+    return;
+  }
+
+  taskTitleInputElem.value = '';
+  const taskList = getItem('tasksList') || [];
+
+  const newTaskList = taskList.concat({
+    text,
+    done: false,
+    createDate: new Date().toISOString(),
+    id: Math.random().toString(),
+  });
+  setItem('tasksList', newTaskList);
+
+  renderTasks();
+};
---
+++ Browser-local-storage/task3/src/scripts/updateTask.js
@@ -0,0 +1,26 @@
+import { renderTasks } from './renderer.js';
+import { getItem, setItem } from './storage.js';
+
+export const onToggleTask = (e) => {
+  const isCheckBox = e.target.classList.contains('list__item-checkbox');
+
+  if (!isCheckBox) {
+    return;
+  }
+  const tasksList = getItem('taskList');
+  const newTaskList = tasksList.map((task) => {
+    if (task.id === e.target.dataset.id) {
+      const done = e.target.checked;
+      return {
+        ...task,
+        done,
+        finishDate: done ? new Date().toISOString() : null,
+      };
+    }
+    return task;
+  });
+
+  setItem('taskList', newTaskList);
+
+  renderTasks();
+};
