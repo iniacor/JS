f84d1b41d7aff0ae400ea3a81bda5467855f6197
ihor <evroigor86@gmail.com>
2022-07-17 03:17:37 +0300

    add change

--- Functions-advanced-approaches/task5/index.js
+++ Functions-advanced-approaches/task5/index.js
@@ -79,17 +79,17 @@ export function shmoment(initValue) {
       return newDataValue;
     },
     result() {
-      return formatter.format(newDataValue);
+      return newDataValue;
     },
   };
   return calculateDate;
 }
 
-// const result = shmoment(new Date(2020, 0, 7, 17, 17, 17))
-//   .add('minutes', 2)
-//   .add('days', 8)
-//   .subtract('years', 1)
-//   .result();
+const result = shmoment(new Date(2020, 0, 7, 17, 17, 17))
+  .add('minutes', 2)
+  .add('days', 8)
+  .subtract('years', 1)
+  .result();
 
 // console.log(result); // => .. Jan 15 2019 17:19:17 ...
 
