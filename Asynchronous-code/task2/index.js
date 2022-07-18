15295afa35b07e1a29170c944b0a69d2417d2a4e
ihor <evroigor86@gmail.com>
2022-07-18 15:07:10 +0300

    add change

--- Asynchronous-code/task2/index.js
+++ Asynchronous-code/task2/index.js
@@ -1,19 +1,17 @@
-'use strict';
-
-/**
- * @param {number} count
- * @param {number} period
- * @return {undefined}
- */
-export const pinger = (count, period) => {
-  let i = count;
-  console.log('Ping');
-  const interval = setInterval(() => {
-    if (--i > 0) {
-      console.log('Ping');
-    } else clearInterval(interval);
-  }, period);
-};
-// test Data
+// /**
+//  * @param {number} count
+//  * @param {number} period
+//  * @return {undefined}
+//  */
+// export const pinger = (count, period) => {
+//   let i = count;
+//   console.log('Ping');
+//   const interval = setInterval(() => {
+//     if (--i > 0) {
+//       console.log('Ping');
+//     } else clearInterval(interval);
+//   }, period);
+// };
+// // test Data
 // pinger(5, 1000); // makes 5 writes with 100 ms interval
 // pinger(7, 150); // makes 7 writes with 1500 ms interval
--- Asynchronous-code/task4/index.js
+++ Asynchronous-code/task4/index.js
@@ -1,8 +1,10 @@
-const sum = (arg1, arg2) => {
-  return arg1 * arg2;
-};
-const delay = (delay, callback, context, ...args) => {
-  setTimeout(() => {}, delay);
+// const sum = (arg1, arg2) => {
+//   return arg1 * arg2;
+// };
+export const delay = (delay, callback, context, ...args) => {
+  setTimeout(() => {
+    callback.call(context, ...args);
+  }, delay);
 };
 
-delay(1000, sum);
+// delay(1000, sum, this, 2, 2);
