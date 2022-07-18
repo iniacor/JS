d62f7e7083b9456f5a90e78202d209e2c95546fe
ihor <evroigor86@gmail.com>
2022-07-18 20:03:13 +0300

    add change

--- Asynchronous-code/task4/index.js
+++ Asynchronous-code/task4/index.js
@@ -1,9 +1,9 @@
-// const sum = (arg1, arg2) => {
-//   return arg1 * arg2;
-// };
+const sum = (arg1, arg2) => {
+  return arg1 * arg2;
+};
 export const delay = (delay, callback, context, ...args) => {
   setTimeout(() => {
-    callback.apply(context, args);
+    console.log(callback.apply(context, args));
   }, delay);
 };
-// console.log(delay(2000, sum, null, 2, 2));
+delay(2000, sum, delay, 2, 2);
