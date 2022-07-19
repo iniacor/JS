accdecc599ee2a284f04f4ba7afb91708217f9ac
ihor <evroigor86@gmail.com>
2022-07-19 23:12:01 +0300

    add change

--- Promise-intro/task7/index.js
+++ Promise-intro/task7/index.js
@@ -1,35 +1,58 @@
-export const asyncCalculator = (numbr) =>
+// export const asyncCalculator = (numbr) =>
+//   new Promise((resolve) => {
+//     resolve(numbr);
+//   })
+//     .then(
+//       (value) =>
+//         new Promise((resolve) => {
+//           setTimeout(() => {
+//             console.log(`Initial value: ${value}`);
+//             resolve(value);
+//           }, 500);
+//         })
+//     )
+//     .then(
+//       (value) =>
+//         new Promise((resolve) => {
+//           if (value === 5) {
+//             setTimeout(() => {
+//               const result = value * value;
+//               console.log(`Squared value: ${result}`);
+//               resolve(result);
+//             }, 500);
+//           }
+//         })
+//     )
+//     .then((value) => {
+//       const result = value * 2;
+//       if (numbr === 5) {
+//         console.log(`Doubled value: ${result}`);
+//       }
+//     });
+
+export const asyncCalculator = (num) =>
   new Promise((resolve) => {
-    resolve(numbr);
+    setTimeout(() => {
+      console.log(`Initial value: ${num}`);
+      resolve(num);
+    }, 500);
   })
     .then(
       (value) =>
         new Promise((resolve) => {
           setTimeout(() => {
-            console.log(`Initial value: ${value}`);
-            resolve(value);
+            const result = value * value;
+            console.log(`Squared value: ${result}`);
+            resolve(result);
           }, 500);
-        })
-    )
-    .then(
-      (value) =>
-        new Promise((resolve) => {
-          if (value === 5) {
-            setTimeout(() => {
-              const result = value * value;
-              console.log(`Squared value: ${result}`);
-              resolve(result);
-            }, 500);
-          }
         })
     )
     .then((value) => {
       const result = value * 2;
-      if (numbr === 5) {
-        console.log(`Doubled value: ${result}`);
-      }
+      console.log(`Doubled value: ${result}`);
+      return result;
     });
 
 // test Data
-asyncCalculator(5);
-asyncCalculator(6);
+// asyncCalculator(5);
+// asyncCalculator(6);
