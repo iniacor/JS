d65264ca478eccd9cb1e06dcd5c439dd00391a5e
ihor <evroigor86@gmail.com>
2022-07-19 12:25:39 +0300

    add change

--- Promise-intro/task1/index.js
+++ Promise-intro/task1/index.js
@@ -19,9 +19,11 @@ export const addImage = (imgSrc) => {
 const resultPromise = addImage(
   'https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg'
 );
-resultPromise.then((data) => {
-  const { width, height } = data;
-  const sizeElem = document.querySelector('.image-size');
-  sizeElem.textContent = `${width} x ${height}`;
-});
-resultPromise.catch((error) => console.log(error));
+resultPromise
+  .then((data) => {
+    console.log(data);
+    const { width, height } = data;
+    const sizeElem = document.querySelector('.image-size');
+    sizeElem.textContent = `${width} x ${height}`;
+  })
+  .catch((error) => console.log(error));
