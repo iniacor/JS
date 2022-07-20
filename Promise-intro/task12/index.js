8b123a9d4626f02a96824f17685e3829c327c897
ihor <evroigor86@gmail.com>
2022-07-20 17:30:05 +0300

    add second task solution

--- Promise-intro/task12/index.js
+++ Promise-intro/task12/index.js
@@ -1,9 +1,31 @@
-export const delay = (delay) =>
+const getRandomNumber = (from, to) => from + Math.random(to - from);
+
+const request = (url) =>
   new Promise((resolve) => {
+    const randomDelay = getRandomNumber(1000, 3000);
     setTimeout(() => {
-      resolve();
-    }, delay);
+      resolve({
+        userData: {
+          name: 'Tom',
+          age: 17,
+        },
+        source: url,
+      });
+    }, randomDelay);
   });
 
+const servers = [
+  'https://server.com/us',
+  'https://server.com/eu',
+  'https://server.com/au',
+];
+export const getUserASAP = (userId) => {
+  const userUrls = servers.map((serverUrl) => `${serverUrl}/${userId}`);
+
+  const requests = userUrls.map((userUrl) => request(userUrl));
+
+  return Promise.race(requests);
+};
+
 // test Data
-delay(3000).then(() => console.log('Done'));
+// getUserASAP('user-id-1').then((result) => console.log(result));
