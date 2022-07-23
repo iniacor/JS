330d12aa581377530aff09c005038729e62e450b
ihor <evroigor86@gmail.com>
2022-07-23 13:22:56 +0300

    add task 2 solution

--- Connecting-whith-server/task4/index.js
+++ Connecting-whith-server/task4/index.js
@@ -1,18 +1,70 @@
-const baseUrl = 'https://5e5cf5eb97d2ea0014796f01.mockapi.io/api/v1/tasks';
-
-export function getTasksList() {
+const baseUrl = 'https://62da9225e56f6d82a7651b64.mockapi.io/api/v1/users';
+export function getUsersList() {
   return fetch(baseUrl).then((response) => response.json());
 }
 
-export function getTaskById(taskId) {
-  return fetch(`${baseUrl}/${taskId}`).then((response) => response.json());
+export function getUserById(userId) {
+  return fetch(`${baseUrl}/${userId}`).then((response) => response.json());
 }
 
-// examples
-getTasksList().then((tasksList) => {
-  console.log(tasksList); // ==> [ {'id':'1', 'isDone':false ... }, {'id':'2', 'isDone':false ... }, ...]
-});
+export function createUser(userData) {
+  return fetch(baseUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8',
+    },
+    body: JSON.stringify(userData),
+  });
+}
 
-getTaskById('2').then((taskData) => {
-  console.log(taskData); // ==> { 'id': '2', 'text': 'District Communications Specialist', 'isDone': false, 'createdDate': 1651499052, 'finishedDate': 1651499052 }
-});
+export function deleteUser(userId) {
+  return fetch(`${baseUrl}/${userId}`, {
+    method: 'DELETE',
+  });
+}
+
+export function updateUser(userId, userData) {
+  return fetch(`${baseUrl}/${userId}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8',
+    },
+    body: JSON.stringify(userData),
+  });
+}
+
+// ====================================
+// test Data
+// getUsersList().then((users) => {
+//   console.log(users); // array of the user objects [{'id':'1', 'firstName':'Grayce' ... }, {'id':'2', 'firstName':'Ara' ... }, ...]
+// });
+
+// getUserById('2').then((userData) => {
+//   console.log(userData); // {'id':'2', 'firstName':'Ara' ... }
+// });
+
+// const newUserData = {
+//   email: 'cool@email.com',
+//   firstName: 'Iron',
+//   lastName: 'Man',
+//   age: 42,
+// };
+
+// createUser(newUserData).then(() => {
+//   console.log('User created');
+// });
+
+// const updatedUserData = {
+//   email: 'new@email.com',
+//   firstName: 'John',
+//   lastName: 'Doe',
+//   age: 17,
+// };
+
+// updateUser('1', updatedUserData).then(() => {
+//   console.log('User updated');
+// });
+
+// deleteUser('2').then(() => {
+//   console.log('User deleted');
+// });
