95feef9fc4bed93efedb07d76ad5950aea22a66c
ihor <evroigor86@gmail.com>
2022-07-25 19:28:12 +0300

    add tasks solution

---
+++ Error-handling/task1/index.js
@@ -0,0 +1,30 @@
+// window.addEventListener('error', function onUnhandledError(err) {
+//   console.error('error', err.message);
+// });
+window.addEventListener('error', function onError(event) {
+  console.log(event.message);
+});
+
+let userParsingResult;
+
+try {
+  const user = JSON.parse('{"name":"Tom"}');
+  console.log('User data: ', user);
+  userParsingResult = 'success';
+} catch (e) {
+  userParsingResult = 'error';
+} finally {
+  console.log(`User parsing finished with ${userParsingResult}`);
+}
+
+let productParsingResult;
+
+try {
+  const product = JSON.parse('{"phone":"ipone"');
+  console.log('Product data: ', product);
+  productParsingResult = 'success';
+} catch (e) {
+  productParsingResult = 'error';
+} finally {
+  console.log(`Product parsing finished with ${productParsingResult}`);
+}
