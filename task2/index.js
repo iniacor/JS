6a117dbdd09607a4a1101629fa0fbb720282061c
ihor <evroigor86@gmail.com>
2022-07-27 14:16:49 +0300

    add second task solution

--- task2/index.js
+++ task2/index.js
@@ -31,19 +31,24 @@ renderUserData(defaultUser);
 const showUserBtnElem = document.querySelector('.name-form__btn');
 const userNameInputElem = document.querySelector('.name-form__input');
 
-const onSearchUser = async () => {
+const onSearchUser = () => {
   cleanReposList();
   showSpinner();
   const userName = userNameInputElem.value;
-  try {
-    const userData = await fetchUserData(userName);
-    renderUserData(userData);
-    const reposList = await fetchRepositories(userData.repos_url);
-    renderRepos(reposList);
-  } catch (error) {
-    alert(err.message);
-  } finally {
-    hideSpinner();
-  }
+  fetchUserData(userName)
+    .then((userData) => {
+      renderUserData(userData);
+      return userData.repos_url;
+    })
+    .then((url) => fetchRepositories(url))
+    .then((reposList) => {
+      renderRepos(reposList);
+    })
+    .catch((err) => {
+      alert(err.message);
+    })
+    .finally(() => {
+      hideSpinner();
+    });
 };
 showUserBtnElem.addEventListener('click', onSearchUser);
---
+++ task2/repos.js
@@ -0,0 +1,17 @@
+const repoList = document.querySelector('.repo-list');
+
+export const cleanReposList = () => {
+  repoList.innerHTML = '';
+};
+
+export const renderRepos = (reposList) => {
+  const reposListElems = reposList.map(({ name }) => {
+    const listElem = document.createElement('li');
+    listElem.classList.add('repo-list__item');
+    listElem.textContent = name;
+
+    return listElem;
+  });
+  cleanReposList();
+  repoList.append(...reposListElems);
+};
