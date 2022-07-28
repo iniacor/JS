6ebaab5ff1d45168f45fe75adc0eb64d79e7ff25
ihor <evroigor86@gmail.com>
2022-07-28 10:56:16 +0300

    add some refactoring

--- Promise-advanced/task3/index.js
+++ Promise-advanced/task3/index.js
@@ -10,14 +10,36 @@
 // return promise with array of users blog
 // cath error if any requre has error
 
-export const getUsersBlogs = async (users) => {
+// export const getUsersBlogs = async (users) => {
+//   try {
+//     const userFetch = users.map((userName) => {
+//       return fetch(`https://api.github.com/users/${userName}`).then(
+//         (response) => response.json().then((url) => url.blog)
+//       );
+//     });
+//     const response = await Promise.all(userFetch);
+//     return response;
+//   } catch (error) {
+//     throw new Error('Failed to load data');
+//   }
+// };
+
+// examples
+// getUsersBlogs(['google', 'facebook', 'reactjs']).then((linksList) =>
+//   console.log(linksList)
+// ); // ==> ["https://opensource.google/", "https://opensource.fb.com", "https://reactjs.org"]
+// getUsersBlogs(['microsoft']).then((linksList) => console.log(linksList)); // ==> ["https://opensource.microsoft.com"]
+
+// =================== Refactoring =========================
+
+export const getUsersBlogs = (users) => {
   try {
-    const userFetch = users.map((userName) => {
-      return fetch(`https://api.github.com/users/${userName}`).then(
-        (response) => response.json().then((url) => url.blog)
-      );
+    const userFetch = users.map(async (userName) => {
+      const response = await fetch(`https://api.github.com/users/${userName}`);
+      const url = await response.json();
+      return url.blog;
     });
-    const response = await Promise.all(userFetch);
+    const response = Promise.all(userFetch);
     return response;
   } catch (error) {
     throw new Error('Failed to load data');
@@ -25,7 +47,9 @@ export const getUsersBlogs = async (users) => {
 };
 
 // examples
-// getUsersBlogs(['google', 'facebook', 'reactjs']).then((linksList) =>
-//   console.log(linksList)
-// ); // ==> ["https://opensource.google/", "https://opensource.fb.com", "https://reactjs.org"]
-// getUsersBlogs(['microsoft']).then((linksList) => console.log(linksList)); // ==> ["https://opensource.microsoft.com"]
+getUsersBlogs(['google', 'facebook', 'reactjs']).then((linksList) =>
+  console.log(linksList)
+); // ==> ["https://opensource.google/", "https://opensource.fb.com", "https://reactjs.org"]
+getUsersBlogs(['microsoft']).then((linksList) => console.log(linksList)); // ==> ["https://opensource.microsoft.com"]
+
+// .then((url) => url.blog)
